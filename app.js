3ccd63746d3ef0a130e90c31c2dd0bd5271fbb09
weichen11235 <44263103+weichen11235@users.noreply.github.com>
2020-04-16 16:41:39 -0400

    change link

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ let cafeArray = [];
 let hallArray = [];
 let clubArray = [];
 
-let getRequest = $.get('http://blmak.github.io/BigMusicClubConclomerateDates.json', function(data, status) {
+let getRequest = $.get('https://blmak.github.io/BigMusicClubConclomerateDates.json', function(data, status) {
   if(status === 'success'){
     data.forEach(function(event){
       if(event.venue === "Little Café"){
